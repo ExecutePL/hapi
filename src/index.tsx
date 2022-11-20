f2a23491ca356a6de5bea53ee40bdcd315d4d441
veronikadumalo <dumalo44084@gmail.com>
2022-11-20 09:44:29 +0100

    feat(scan): add view

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ import {
   ApolloProvider,
   gql,
 } from "@apollo/client";
+import { CodeReader } from "view/CodeReader";
 
 const client = new ApolloClient({
   uri: "https://hapibe.herokuapp.com/graphql",
@@ -47,6 +48,10 @@ const router = createBrowserRouter([
   {
     path: "/add-sensor/:serialNumber",
     element: <AddSensor />,
+  },
+  {
+    path: "/scan",
+    element: <CodeReader />,
   },
 ]);
 
---
+++ src/view/CodeReader.tsx
@@ -0,0 +1,38 @@
+import { Box, Typography } from "@mui/material";
+import { QrReader } from "react-qr-reader";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Layout } from "components/Layout";
+
+export const CodeReader = () => {
+  const [data, setData] = useState<string | null>(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!data) return;
+    const isSerialNumber = data.includes("serialNumber");
+    if (isSerialNumber) {
+      const serialNumber = data.slice(13, data.length + 1);
+      navigate(`/add-sensor/${serialNumber}`);
+    }
+  }, [data]);
+
+  return (
+    <Layout>
+      <Typography color="secondary" fontSize="18px" sx={{ padding: "15px" }}>
+        Please, scan qr code
+      </Typography>
+      <Box sx={{ width: "100%", height: "100%" }}>
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData((result as any)?.text);
+            }
+          }}
+          constraints={{
+            facingMode: "environment",
+          }}
+        />
+      </Box>
+    </Layout>
+  );
+};
